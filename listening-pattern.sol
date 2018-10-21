a58568ef65c8773a6216f294cc2d445060bdd55f
Shaun Azzopardi <shaun.azzopardi@gmail.com>
2018-10-21 19:42:30 +0200

    extended pattern to allow for registration and de-registration

--- listening-pattern.sol
+++ listening-pattern.sol
@@ -4,13 +4,15 @@ interface Listener{
 
 contract Notifier{
 
+  mapping(address => boolean) listening;
   address[] listeners;
   
   function funcName(){
       //some action
       
       for(uint i = 0; i < listeners.length; i++){
-          if(listeners[i] > 0){
+          if(listeners[i] > 0
+                            && listening[listeners[i]]){
               (Listener(listeners[i])).notify();
           }
       }
@@ -18,6 +20,11 @@ contract Notifier{
   
   function register(){
       listeners.push(msg.sender);
+      listening[msg.sender] = true;
+  }
+  
+  function deRegister(){
+      listening[msg.sender] = false;
   }
 
 }
